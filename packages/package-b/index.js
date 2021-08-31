c749e32d05de90bb3e40b537dd8f8cca3b3a326a
zqinmiao <zqinmiao@gmail.com>
2021-08-31 16:44:43 +0800

    chore: testest

--- packages/package-b/index.js
+++ packages/package-b/index.js
@@ -1,6 +1,6 @@
 const packageA = require("@buibis/package-a");
 
 const _ = require("lodash");
-console.log("a22", packageA, packageA.a);
+console.log("a222", packageA, packageA.a);
 
 console.log("isEmpty", _.isEmpty(""));
