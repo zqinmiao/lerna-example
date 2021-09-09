5c2ce08fdc0794eff1b6a37a3d03c6c466be0fba
zqinmiao <zqinmiao@gmail.com>
2021-09-09 16:23:51 +0800

    feat: update package-b

--- packages/package-b/index.js
+++ packages/package-b/index.js
@@ -1,6 +1,6 @@
 const packageA = require("@buibis/package-a");
 
 const _ = require("lodash");
-console.log("a222", packageA, packageA.a);
+console.log("a222-b", packageA, packageA.a);
 
 console.log("isEmpty", _.isEmpty(""));
